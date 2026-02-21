faf0bc6e39cbbdbd21c689a42123d62a89f425d1
komron <komronbekmansurov1@gmail.com>
2026-02-21 22:49:53 +0500

    yangi loyiha

--- category/admin.py
+++ category/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Category
 
-@admin.register(Category)
+admin.site.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'brand', 'is_active')
